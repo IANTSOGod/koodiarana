b983b048cbe3178c6a648559ed625c56fc3cf841
IANTSOGod <iantsochristianrazafindrazaka0gmail.com>
2025-01-21 10:06:50 +0300

    commit all

--- src/routes/email.route.ts
+++ src/routes/email.route.ts
@@ -23,7 +23,17 @@ router.post("/sendVerificationLink", async (req: Request, res: Response) => {
   const { token } = req.body;
   const payload = verifyToken(token) as { email: string };
   try {
-  } catch (error) {}
+    const mailOptions={
+      from: "iantsochristianrazafindrazaka@gmail.com", // Adresse email de l'expéditeur
+      to: payload.email, // Adresse email du destinataire
+      subject: "Email verification", // Sujet de l'email
+      text: `Voici le lien de vérification de compte http://localhost:3000/users/verifyEmail/${token}`, // Texte brut      
+    }
+    const info=await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: "Email envoyé" });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 export default router;
