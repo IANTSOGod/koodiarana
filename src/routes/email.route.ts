e9c41caa561005c0932b867c45e32d9d3a62eec6
IANTSOGod <iantsochristianrazafindrazaka0gmail.com>
2025-01-22 15:54:04 +0300

    finishing images handling

--- src/routes/email.route.ts
+++ src/routes/email.route.ts
@@ -1,12 +1,13 @@
 import { Router, Request, Response } from "express";
 import { transporter } from "../config/mailer";
-import { generateToken, verifyToken } from "../config/jwt";
+import { verifyToken } from "../config/jwt";
 import { configDotenv } from "dotenv";
 import { generateOtp } from "../config/Otp";
 
 configDotenv({ path: ".env" });
 
 const router = Router();
+const domain = process.env.DOMAIN_NAME;
 
 router.post("/send", async (req: Request, res: Response) => {
   const { to, subject, text } = req.body;
@@ -35,7 +36,7 @@ router.post("/sendVerificationLink", async (req: Request, res: Response) => {
       from: societyEmail, // Adresse email de l'expéditeur
       to: payload.email, // Adresse email du destinataire
       subject: "Email verification", // Sujet de l'email
-      text: `Voici le lien de vérification de compte http://localhost:3000/users/verifyEmail/${token}`, // Texte brut
+      text: `Voici le lien de vérification de compte ${domain}/users/verifyEmail/${token}`, // Texte brut
     };
     const info = await transporter.sendMail(mailOptions);
     res.status(200).json({ message: "Email envoyé" });
@@ -61,7 +62,5 @@ router.post("/sendOTP", async (req: Request, res: Response) => {
     res.status(500).json(error);
   }
 });
-
-
 
 export default router;
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,9 +3,101 @@ import User from "../schema/user";
 import { hash } from "bcrypt";
 import { generateToken, verifyToken } from "../config/jwt";
 import { verifyOtp } from "../config/Otp";
+import multer from "multer";
+import path from "path";
+import { configDotenv } from "dotenv";
 
 const router = Router();
+configDotenv({ path: ".env" });
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // Vérifie la route et définit le dossier approprié
+    if (req.path === "/uploadProfilePics") {
+      cb(null, path.join(__dirname, "../assets/profile"));
+    } else if (req.path === "/uploadCIN1") {
+      cb(null, path.join(__dirname, "../assets/cinFront"));
+    } else if (req.path === "/uploadCIN2") {
+      cb(null, path.join(__dirname, "../assets/cinBack"));
+    } else {
+      cb(new Error("Route non valide pour l’upload"), "");
+    }
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, `${uniqueSuffix}-${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+const domain = process.env.DOMAIN_NAME;
+
+router.post(
+  "/uploadProfilePics",
+  upload.single("file"),
+  async (req: Request, res: Response) => {
+    if (!req.file) {
+      res.status(404).json({ message: "Aucun fichier fournit" });
+    } else {
+      const { email } = req.body;
+      try {
+        const user = await User.findOne({ email: email });
+        if (user) {
+          user.photoProfil = `${domain}/assets/profile/` + req.file.filename;
+          await user.save();
+          res.status(200).json({ message: "Photo de profil téléchargée" });
+        }
+      } catch (error) {
+        res.status(500).json({ error });
+      }
+    }
+  }
+);
+
+router.post(
+  "/uploadCIN1",
+  upload.single("file"),
+  async (req: Request, res: Response) => {
+    if (!req.file) {
+      res.status(404).json({ message: "Aucun fichier fournit" });
+    } else {
+      const { email } = req.body;
+      try {
+        const user = await User.findOne({ email: email });
+        if (user) {
+          user.photoCIN1 = `${domain}/assets/cinFront/` + req.file.filename;
+          await user.save();
+          res.status(200).json({ message: "Photo de CIN avant téléchargée" });
+        }
+      } catch (error) {
+        res.status(500).json({ error });
+      }
+    }
+  }
+);
+
+router.post(
+  "/uploadCIN2",
+  upload.single("file"),
+  async (req: Request, res: Response) => {
+    if (!req.file) {
+      res.status(404).json({ message: "Aucun fichier fournit" });
+    } else {
+      const { email } = req.body;
+      try {
+        const user = await User.findOne({ email: email });
+        if (user) {
+          user.photoCIN2 = `${domain}/assets/cinBack/` + req.file.filename;
+          await user.save();
+          res.status(200).json({ message: "Photo de CIN apres téléchargée" });
+        }
+      } catch (error) {
+        res.status(500).json({ error });
+      }
+    }
+  }
+);
 router.post("/create", async (req: Request, res: Response) => {
   const { nom, prenom, dateNaissance, email, num, password, status } = req.body;
   const hashedPassword = await hash(password, 10);
@@ -80,6 +172,5 @@ router.post("/sendResetPasswordLink", async (req: Request, res: Response) => {
     res.status(500).json({ error });
   }
 });
-
 
 export default router;
